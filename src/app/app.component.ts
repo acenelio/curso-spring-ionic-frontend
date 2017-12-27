5ca41a9ab19a315e3179b06e701361c4d96cf99b
Nelio Alves <acenelio@gmail.com>
2017-12-27 16:51:14 -0200

    Criando uma pagina de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' }
     ];
 
   }
---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../services/storage.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  email: string;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.email = localUser.email;
+    }
+  }
+}
