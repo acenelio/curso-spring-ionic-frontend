62dfbb99325a9f3c8eef913d0f0dd186dcb152b4
Nelio Alves <acenelio@gmail.com>
2017-12-28 13:20:48 -0200

    Criando pagina de Signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -48,4 +48,8 @@ export class HomePage {
       },
       error => {});    
   }
+ 
+  signup() {
+    this.navCtrl.push('SignupPage');
+  }
 }
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  signupUser() {
+    console.log("enviou o form");
+  }
+}
