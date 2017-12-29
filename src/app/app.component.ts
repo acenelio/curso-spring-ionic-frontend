0c2347701dcb22bd44484fbad9e3f6f567529e4c
Nelio Alves <acenelio@gmail.com>
2017-12-29 16:39:55 -0200

    Acesso ao carrinho: botao flutuante e menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Carrinho', component: 'CartPage'},
       { title: 'Logout', component: ''}
     ];
 
