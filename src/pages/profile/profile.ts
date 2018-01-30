a2b13394fdd5cc1e9e0dd824d70452cee00bd096
Nelio Alves <acenelio@gmail.com>
2018-01-30 15:45:21 -0200

    Obtendo imagem da galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -74,6 +74,25 @@ export class ProfilePage {
     });
   }
 
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
+  }
+
   sendPicture() {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
